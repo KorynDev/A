c43c590f1a41c42f0adba9ab52f130c83214e2a4
KorynDev <tysonholmes2010@gmail.com>
2026-08-20 22:01:33 +0100

    Add DrawGround function to render the ground; update main loop to call DrawGround

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,8 +1,19 @@
 #include "main.hpp"
 
+auto DrawGround() -> void {
+    for (int x = 0; x < W_WIDTH; x++) { 
+        if (x >= W_WIDTH) x = W_WIDTH;
+        for (int y = 0; y < W_HEIGHT; y++) {
+            if (y >= W_HEIGHT) y = W_HEIGHT;
+            DrawRectangle(x, y, 50, 50, BROWN);
+        }
+    }
+}
+
 int main() {
     InitWindow(W_WIDTH, W_HEIGHT, W_TITLE);
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
+    SetExitKey(KEY_Q);
     // gonna test load smth;
     loadalltextures();
     Player plr;
@@ -12,6 +23,7 @@ int main() {
         float dt = GetFrameTime();
         BeginDrawing();
         ClearBackground(W_COLOR);
+        DrawGround();
         plr.draw_health_bar();
         EndDrawing();
     }
